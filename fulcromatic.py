cc9b26678654a17560083ae65ab5bb5df0f44ce9
Kyle Smith <kwsmith@sdsu.edu>
2020-09-24 11:59:37 -0700

    Get 360 photos

--- fulcromatic.py
+++ fulcromatic.py
@@ -10,25 +10,10 @@ print('Welcome, my friend.  Libraries imported.')
 fulcrum = Fulcrum(key=os.environ['FULCRUM_API'])
 print('\n Fulcrum API connection established.')
 
-db_host = "postgresqlsdsu.postgres.database.azure.com"
-db_name = "fac_data_warehouse"
-db_user = os.environ['AZURE_PG_UKEY']
-db_password = os.environ['AZURE_PG_PW']
-sslmode = "require"
-#conn_string = "host={0} user={1} dbname={2} password={3} sslmode={4}".format(db_host, db_user, db_name, db_password, sslmode)
-#sql_engine = psycopg2.connect(conn_string) 
-print("Azure Connection established"+'\n' + '=================================')  
 
-#Production Engine
-#engine = sqlalchemy.create_engine('postgresql+psycopg2://{}:{}@postgresqlsdsu.postgres.database.azure.com/fac_data_warehouse'.format(db_user, db_password))
+export_dir = r'C:\Data\fmatic'
 
-#Local Test Engine
-engine = sqlalchemy.create_engine('postgresql+psycopg2://postgres:Prost@780@localhost/fac_data_warehouse')
-
-#Connect to Engine
-conn = engine.connect() 
-
-
+os.chdir(export_dir)
 
 
 db_host = "postgresqlsdsu.postgres.database.azure.com"
@@ -36,18 +21,14 @@ db_name = "fac_data_warehouse"
 db_user = os.environ['AZURE_PG_UKEY']
 db_password = os.environ['AZURE_PG_PW']
 sslmode = "require"
-#conn_string = "host={0} user={1} dbname={2} password={3} sslmode={4}".format(db_host, db_user, db_name, db_password, sslmode)
-#sql_engine = psycopg2.connect(conn_string) 
-print("Azure Connection established"+'\n' + '=================================')  
+
+
 
 #Production Engine
-#engine = sqlalchemy.create_engine('postgresql+psycopg2://{}:{}@postgresqlsdsu.postgres.database.azure.com/fac_data_warehouse'.format(db_user, db_password))
+engine = sqlalchemy.create_engine('postgresql+psycopg2://{}:{}@postgresqlsdsu.postgres.database.azure.com/fac_data_warehouse'.format(db_user, db_password))
+print("Azure Connection established"+'\n' + '=================================')  
 
-#Local Test Engine
-engine = sqlalchemy.create_engine('postgresql+psycopg2://postgres:Prost@780@localhost/fac_data_warehouse')
 
-#Connect to Engine
-conn = engine.connect() 
 
 sql='''
 SELECT
@@ -92,39 +73,103 @@ WHERE room_key IS NOT NULL;
 
 
 
-df = pd.read_sql(sql, engine, index_col='_title')
+print('Fetching unprocesed surveys from FDW\n')
+df = pd.read_sql(sql, engine)
 df['room_key'] = df['room_key'].astype('int')
+df.to_csv('records.csv')
 
+print('Converting to dictionary\n')
 df = df.to_dict(orient='records')
 
-os.chdir(r'C:\Data\fmatic')
 
+
+photos_to_process = ['signage_photos', '360_photo']
+
+
+
+print('\n---------- Processing signage photos ----------\n')
 
 for i in df:
-    print(i)
+    
     if i['signage_photos'] != None:
+        print('\n' + i['_title'] + ' contains ' + str(len(i['signage_photos'].split(','))) + ' photos.')
+
+        #Check to see if there's already a dir for the given room.  If not, make it.  
         if not os.path.exists(str(i['room_key'])):
             os.mkdir(str(i['room_key']))
             os.chdir(str(i['room_key']))
-            print(os.getcwd())
+            print('Created directory:' + os.getcwd())
+        
             if not os.path.exists('signage_photos'):
                 os.mkdir('signage_photos')
                 os.chdir('./signage_photos')
-                print('Signage photos directory created.')
-            else:
-                pass
+                print('Signage photos directory created.') 
+                
         else:
             pass
 
         for rec in i['signage_photos'].split(','):
             
-            file_name = i['sfdb_number'] + '-' + i['room_number'] + '-signage_' + rec[0:4]
-            print(str(i['room_key']) + '-' + rec)
+            #Get the index position of the photo
+            index_pos = i['signage_photos'].split(',').index(rec)
+            print('Image index position: ' + str(index_pos))
+
+            #Add a suffix as a means of dealing with multiple photos so they don't have duplicate names
+            suffix = index_pos + 1
+            print('Suffix: ' + str(suffix))
+
+            if i['signage_photos_captions'] == None or i['signage_photos_captions'].split(',')[index_pos] == '':
+                print('No captions.  Using default.')
+                file_name = i['sfdb_number'] + '-' + i['room_number'] + '-signage-' + str(suffix)
+            
+            elif i['signage_photos_captions'] != None:
+                print('Images contain captions.')
+                print(i['signage_photos_captions'].split(',')[index_pos])
+                file_name =  i['_title'] + '-' + str(i['signage_photos_captions'].split(',')[index_pos])
+            
+            else:
+                file_name = i['sfdb_number'] + '-' + i['room_number'] + '-signage-' + str(suffix)
+
+            
+            
+            
+            #Get the photo object from Fulcrum API
             photo = fulcrum.photos.media(rec)
             with open(f'{file_name}.jpg', 'wb') as f:
                 f.write(photo)
-            
+            print(f'{file_name}.jpg' + ' saved')
+        
+        print('---------- Finished with signage photos! ----------\n')
+        os.chdir(export_dir)
 
-        os.chdir(r'C:\Data\fmatic')
+    #Get 360 photo
+    print('\nGetting 360 photo')
+
+    if i['360_photo'] != None:
+        rkey = str(i['room_key'])
+        
+        if not os.path.exists(str(i['room_key'])):
+            os.mkdir(str(i['room_key']))
+            os.chdir(str(i['room_key']))
+            print('Created directory:' + os.getcwd())
+        
+        os.chdir(os.path.join(export_dir, str(rkey)))
+        print(os.getcwd())
+        os.mkdir('./360_photo')
+        
+        os.chdir('./360_photo')
+        print(os.getcwd())
+        
+
+        file_name = i['sfdb_number'] + '-' + i['room_number'] + '-360'
+
+        photo = fulcrum.photos.media(i['360_photo'])
+        with open(f'{file_name}.jpg', 'wb') as f:
+            f.write(photo)
+        print(f'{file_name}.jpg' + ' saved')
+        print('---------- Finished with 360 photo! ----------\n')
+
+
+    os.chdir(export_dir)
        
     
